dd716670b5b6e8661c39aa5b805bd2e8a6b0198f
Shoma Nisimura <e195737@ie.u-ryukyu.ac.jp>
2020-01-20 01:16:18 +0900

    とりあえずサイコロを３回ふった。

---
+++ src/main/java/jp/ac/uryukyu/e195737/Dice.java
@@ -0,0 +1,15 @@
+package jp.ac.uryukyu.e195737;
+import java.util.Arrays;
+import java.util.Random;
+
+public class Dice {
+
+    public static void main(String[] args) {
+        int[] dices = new int[3];
+        for (int i = 0; i < dices.length; i++) {
+            dices[i] = (int) (Math.random() * 6) + 1;
+        }
+        String list = Arrays.toString(dices);
+        System.out.println(list);
+    }
+}
